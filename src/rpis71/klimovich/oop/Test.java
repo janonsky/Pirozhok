bafcc74a61f3b34f6b42443e6066ca3b06aa5769
Janonsky <ruslan.klimovich@yandex.ru>
2019-03-04 23:57:22 +0300

    Added entry point

--- src/rpis71/klimovich/oop/Test.java
+++ src/rpis71/klimovich/oop/Test.java
@@ -1,7 +1,20 @@
 package rpis71.klimovich.oop;
-
+import rpis71.klimovich.oop.model.Account;
 public class Test {
     public static void main (String[] args) {
         System.out.println("Я сделяль!");
     }
+
+    public static void lab1tests()
+    {
+        Account userOne=new Account("chmo",100);
+        Account userTwo=new Account("daun",1000);
+        Account userThree=new Account("loh",10000);
+        Account userFour=new Account("eblan",100000);
+        Account userFife=new Account("nishiy",0);
+        Account[] accounts=new Account[5];
+        //Account[] accounts={userOne,userTwo,userThree,userFour,userFife};
+
+    }
 }
+
