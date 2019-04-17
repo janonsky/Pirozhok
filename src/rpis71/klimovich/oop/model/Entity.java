14d18404f6f87a31e456f18fa178b5546e23bcd2
Janonsky <ruslan.klimovich@yandex.ru>
2019-04-17 16:32:33 +0300

    add metods

--- src/rpis71/klimovich/oop/model/Entity.java
+++ src/rpis71/klimovich/oop/model/Entity.java
@@ -2,6 +2,15 @@ package rpis71.klimovich.oop.model;
 
 public class Entity implements Client {
 
+    public class Node {
+        Node next;
+        Account value;
+
+        public Node(Account value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
     private Node head;
     private Node tail;
     private String name;
@@ -19,71 +28,149 @@ public class Entity implements Client {
 
     @Override
     public Boolean add(Account account) {
-        return null;
+        //добовление в конец
+        Node node = new Node(account, null);
+        if (tail==null) head=tail=node;
+        else {
+            node.next=tail.next;
+            tail.next=node;
+            tail=node;
+        }
+        size++;
+        return true;
     }
 
+    private Node getNode(int index){
+        int numderNode=1;
+        Node currentNode=head.next;
+        while (numderNode<index)
+        {
+            currentNode=currentNode.next;
+            numderNode++;
+        }
+        return currentNode.next;
+
+    }
     @Override
     public Boolean add(int index, Account account) {
+        Node node = new Node(account, null), nextNode,currentNode;
+        if (tail == null) {
+            head.next = node;
+            tail = node;
+        } else if (index == 0) return false;
+        else if (index == 1)  //уточнить считается ли head за элемент списка
+        {
+            nextNode = head.next;
+            node.next = nextNode;
+            head.next = node;
+        } else if (index == size) {
+            node.next = head;
+            tail.next = node;
+            tail = node;
+        } else
+        {
+
+        }
         return null;
     }
 
     @Override
     public Account get(int index) {
-        return null;
+        return getNode(index).value;
     }
 
+    private Node getNodeByNumber(String accountNumber) {
+        Node node = head;
+        Node temp=null;
+        int index = 1;
+        while (index <= size) {
+            if (node.value.getNumber().equals(accountNumber))
+            {
+                temp = node;
+                break;
+            }
+            index++;
+            node= node.next;
+        }
+    return temp;
+    }
     @Override
     public Account get(String accountNumber) {
-        return null;
+        return getNodeByNumber(accountNumber).value;
     }
 
     @Override
     public Boolean hasAccount(String accountNumber) {
-        return null;
+        return (getNodeByNumber(accountNumber)!=null);
     }
 
     @Override
     public Account set(int index, Account account) {
+       Account removedAccount=get(index);
+       Node node= getNode(index);
+       node.value=account;
+       return removedAccount;
+    }
+
+    @Override
+    public Account remove(int index) { //hz
         return null;
     }
 
     @Override
-    public Account remove(int index) {
-        return null;
-    }
-
-    @Override
-    public Account remove(String accountNumber) {
+    public Account remove(String accountNumber) { //hz
         return null;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public Account[] getAccounts() {
-        return new Account[0];
+        Node node=head;
+        Account[] accounts=new Account[size];
+        for(int i=0;i<size;i++)
+        {
+            accounts[i]=node.value;
+            node=node.next;
+        }
+        return accounts;
     }
 
     @Override
     public Account[] sortedAccountByBalance() {
-        return new Account[0];
+        Account[] sortedAccountsByBalance = getAccounts();
+        Account account;
+        for (int i = 0; i < size - 1; i++)
+            for (int j = 0; j < size - i; j++)
+                if (sortedAccountsByBalance[j].getBalance() > sortedAccountsByBalance[j + 1].getBalance()) {
+                    account = sortedAccountsByBalance[j];
+                    sortedAccountsByBalance[j] = sortedAccountsByBalance[j + 1];
+                    sortedAccountsByBalance[j + 1] = account;
+                }
+        return sortedAccountsByBalance;
     }
-
     @Override
     public double totalBalance() {
-        return 0;
+       double totalBalance=0;
+       Node node=head;
+       for(int i=0;i<size;i++)
+       {
+           totalBalance+=node.value.getBalance();
+           node=node.next;
+       }
+       return totalBalance;
     }
 
     @Override
     public String getName() {
-        return null;
+        return name;//??????
     }
 
     @Override
     public void setName(String name) {
-
+        this.name=name;//????????
     }
 }
